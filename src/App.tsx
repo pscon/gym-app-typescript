199369756bbb6d16a2edb976b443343094c26de9
pscon <ekunolapaul@gmail.com>
2023-05-19 07:53:29 +0100

    Feat: developed the navbar, home and benefits component of the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Navbar from "@/scenes/navbar"
+import { SelectedPage } from '@/shared/types';
+import Home from '@/scenes/home';
+import Benefits from '@/scenes/benefits';
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  
 
   return (
+
     <>
-    
-    <div className='bg-gray-20'>
-App
-    </div>
-        </>
+
+      <div className=''>
+        <Navbar setSelectedPage={setSelectedPage}
+          selectedPage={selectedPage} isTopOfPage={isTopOfPage} />
+        <Home setSelectedPage={setSelectedPage} />
+        <Benefits setSelectedPage={setSelectedPage} />
+
+      </div>
+    </>
   )
 }
 
---
+++ src/shared/types.ts
@@ -0,0 +1,12 @@
+export enum SelectedPage {
+    Home = "home",
+    Benefits = "benefits",
+    OurClasses = "ourClasses",
+    ContactUs = "contactUs",
+    }
+
+    export interface BenefitType {
+        icon: JSX.Element;
+        title: string;
+        description: string;
+    }
